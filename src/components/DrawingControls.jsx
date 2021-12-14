46f97fc976d4bd935f1b0fd1f372e5daa6cc1971
Dylan Mikus <dbmikus@gmail.com>
2021-12-13 19:23:51 -0800

    Add half-dot for selected drawing tool

--- src/components/DrawingControls.jsx
+++ src/components/DrawingControls.jsx
@@ -1,37 +1,54 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import styled from 'styled-components';
 import ColorPickerContainer from './ColorPicker';
 import EraserContainer from './Eraser';
 import BucketContainer from './Bucket';
 import MoveContainer from './Move';
 import EyedropperContainer from './Eyedropper';
+import * as drawingToolStates from '../store/reducers/drawingToolStates';
 import { colors } from '../utils/color';
 
-function DrawingControls({ helpOn, className }) {
+function DrawingControls({ selectedTool, helpOn, className }) {
   return (
     <div className={`${className} app__mobile--group`}>
-      <BucketContainer
-        helpOn={helpOn}
-        helpTooltip="It fills an area of the current frame based on color similarity (B)"
-      />
-      <EyedropperContainer
-        helpOn={helpOn}
-        helpTooltip="Sample a color from your drawing (O)"
-      />
-      <ColorPickerContainer
-        helpOn={helpOn}
-        helpTooltip="Choose a new color that is not in your palette (P)"
-      />
-      <EraserContainer helpOn={helpOn} helpTooltip="Remove colors (E)" />
-      <MoveContainer
-        helpOn={helpOn}
-        helpTooltip="Move your drawing around the canvas (M)"
-      />
+      <ToolWrapper isSelected={selectedTool === drawingToolStates.BUCKET}>
+        <BucketContainer
+          helpOn={helpOn}
+          helpTooltip="It fills an area of the current frame based on color similarity (B)"
+        />
+      </ToolWrapper>
+      <ToolWrapper isSelected={selectedTool === drawingToolStates.EYEDROPPER}>
+        <EyedropperContainer
+          helpOn={helpOn}
+          helpTooltip="Sample a color from your drawing (O)"
+        />
+      </ToolWrapper>
+      <ToolWrapper isSelected={selectedTool === drawingToolStates.COLOR_PICKER}>
+        <ColorPickerContainer
+          helpOn={helpOn}
+          helpTooltip="Choose a new color that is not in your palette (P)"
+        />
+      </ToolWrapper>
+      <ToolWrapper isSelected={selectedTool === drawingToolStates.ERASER}>
+        <EraserContainer helpOn={helpOn} helpTooltip="Remove colors (E)" />
+      </ToolWrapper>
+      <ToolWrapper isSelected={selectedTool === drawingToolStates.MOVE}>
+        <MoveContainer
+          helpOn={helpOn}
+          helpTooltip="Move your drawing around the canvas (M)"
+        />
+      </ToolWrapper>
     </div>
   );
 }
 
-const StyledDrawingControls = styled(DrawingControls)`
+const mapStateToProps = state => ({
+  selectedTool: state.present.get('drawingTool')
+});
+const ConnectedDrawingControls = connect(mapStateToProps)(DrawingControls);
+
+const StyledDrawingControls = styled(ConnectedDrawingControls)`
   text-align: center;
   margin: 1em 0 0.6em;
   display: flex;
@@ -53,6 +70,51 @@ const StyledDrawingControls = styled(DrawingControls)`
       margin: 0;
     }
   }
+`;
+
+function halfCircleCss(size, color) {
+  const doubleSize = size * 2;
+  const ret = `
+        background-color: ${color};
+        display: inline-block;
+        height: ${doubleSize}px;
+        width: ${size}px;
+        border-bottom-right-radius: ${doubleSize}px;
+        border-top-right-radius: ${doubleSize}px;
+    `;
+  return ret;
+}
+
+const ToolWrapper = styled.div.attrs(() => ({
+  size: 7,
+  color: 'white'
+}))`
+  position: relative;
+
+  ${props =>
+    props.isSelected
+      ? `&:before {
+        content: '';
+
+        ${halfCircleCss(props.size, props.color)};
+
+        // height: 10px;
+        // width: 10px;
+        // border-radius: 50%;
+        // display: inline-block;
+
+        position: absolute;
+        transform: translate(-50%, -50%);
+        left: -0.6em;
+        top: 50%;
+
+        @media only screen and (max-width: 740px) {
+            transform: translate(-50%, -50%) rotate(90deg);
+            left: 50%;
+            top: -0.6em;
+        }
+    }`
+      : null}
 `;
 
 export default StyledDrawingControls;
