5cc5b5ba5a976508fe3c90c1df0f075265823e52
Dylan Mikus <dbmikus@gmail.com>
2021-12-17 15:02:48 -0800

    Require Pixly name

--- src/components/RightControls.jsx
+++ src/components/RightControls.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import { setFrameName } from '../store/actions/actionCreators';
@@ -21,6 +21,10 @@ function RightControls({
   setPixlyName,
   pixlyName
 }) {
+  const [errorMsg, setErrorMsg] = useState(null);
+  console.log('error message');
+  console.log(errorMsg);
+
   return (
     <div className={`app__right-side ${className}`}>
       <RightControlsInnerContainer>
@@ -38,16 +42,31 @@ function RightControls({
             <MintButton
               type="button"
               ariaLabel="Mint Pixly"
-              onClick={mintClickFn}
+              onClick={() => {
+                if (pixlyName) {
+                  setErrorMsg(null);
+                  mintClickFn();
+                } else {
+                  setErrorMsg('Pixly name required.');
+                }
+              }}
             >
               MINT PIXLY!
             </MintButton>
+            {errorMsg ? <ErrorMessage>{errorMsg}</ErrorMessage> : null}
           </div>
         </div>
       </RightControlsInnerContainer>
     </div>
   );
 }
+
+const ErrorMessage = styled.div`
+  color: red;
+  font-size: 1.2em;
+  margin-top: 0.5em;
+  text-align: center;
+`;
 
 const mapStateToProps = state => {
   const frames = state.present.get('frames');
